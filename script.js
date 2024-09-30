37d24312d05fc96fef807197685864e156efc413
Leonardo Henrique <code.leoh@gmail.com>
2024-09-30 18:58:15 -0300

    Exibindo a primeira questão e as suas opções

--- script.js
+++ script.js
@@ -8,7 +8,17 @@ function showQuestion() {
     if(questions[currentQuestion]) {
         let q = questions[currentQuestion];
 
-        console.log(q.question);
+        document.querySelector('.scoreArea').style.display = 'none';
+        document.querySelector('.questionArea').style.display = 'block';
+
+        document.querySelector('.question').innerHTML = q.question;
+        
+        let optionsHtml = '';
+        for(let i in q.options) {
+            optionsHtml += `<div class="option"><span>${i}</span> ${q.options[i]}</div>`;
+        }
+        document.querySelector('.options').innerHTML = optionsHtml;
+
     } else {
         // Acabaram as questões
     }
