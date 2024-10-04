aa4ad343b4cd358170e19388489084a2653e6f65
Leonardo Henrique <code.leoh@gmail.com>
2024-10-04 20:10:50 -0300

    Botão de reset criado

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let correctAnswers = 0;
 
 showQuestion();
 
+// Events
+document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
+
 // Functions
 function showQuestion() {
     if(questions[currentQuestion]) {
@@ -63,4 +66,10 @@ function finishQuiz() {
     document.querySelector('.scoreArea').style.display = 'block';
     document.querySelector('.questionArea').style.display = 'none';
     document.querySelector('.progress--bar').style.width = `100%`;
+}
+
+function resetEvent() {
+    correctAnswers = 0;
+    currentQuestion = 0;
+    showQuestion();
 }
